9a292007034919b1cd13fb71fa262617e5a352a9
takahiko03 <takahiko03@ff722a34-1543-11df-bbb9-3d7b7cea6d78>
2010-06-11 07:10:50 +0000

    [trunk] - commited changes needed to upgraded cassandra.
    git-svn-id: https://likelike.googlecode.com/svn/trunk@69 ff722a34-1543-11df-bbb9-3d7b7cea6d78

--- src/core/org/unigram/likelike/writer/CassandraWriter.java
+++ src/core/org/unigram/likelike/writer/CassandraWriter.java
@@ -1,19 +1,19 @@
 package org.unigram.likelike.writer;
 
-import static me.prettyprint.cassandra.utils.StringUtils.bytes;
 import me.prettyprint.cassandra.service.CassandraClient;
 import me.prettyprint.cassandra.service.CassandraClientPool;
 import me.prettyprint.cassandra.service.CassandraClientPoolFactory;
 import me.prettyprint.cassandra.service.Keyspace;
 import me.prettyprint.cassandra.service.PoolExhaustedException;
 
-import org.apache.cassandra.service.ColumnPath;
-import org.apache.cassandra.service.InvalidRequestException;
-import org.apache.cassandra.service.NotFoundException;
-import org.apache.cassandra.service.TimedOutException;
-import org.apache.cassandra.service.UnavailableException;
+import org.apache.cassandra.thrift.ColumnPath;
+import org.apache.cassandra.thrift.InvalidRequestException;
+import org.apache.cassandra.thrift.NotFoundException;
+import org.apache.cassandra.thrift.TimedOutException;
+import org.apache.cassandra.thrift.UnavailableException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.mapreduce.Reducer.Context;
+
 import org.apache.thrift.TException;
 
 public class CassandraWriter implements IWriter {
@@ -38,11 +38,13 @@ public class CassandraWriter implements IWriter {
     }
 
     public boolean write(Long key, Long value, Context context) 
-    throws UnavailableException, InvalidRequestException, TException, TimedOutException {
-        ColumnPath cp =  new ColumnPath(this.columnFamily, null, 
-                value.toString().getBytes());     
+    throws UnavailableException, InvalidRequestException, TException, 
+    	TimedOutException {
+        ColumnPath cp =  new ColumnPath(this.columnFamily);
+        cp.setColumn(value.toString().getBytes());
+        System.out.println("key: " + key + "\tvalue: " + value);
         this.keySpace.insert(key.toString(), cp, new Long(1).toString().getBytes());       
-        return false;
+        return true;
     }
     
     /** symbol: cassandra server name. */
